c4b0c009c93f4ffd75f0a482635263613279734d
eris <eris.koleszar@gmail.com>
2023-03-10 15:36:25 -0800

    Add developer settings template

---
+++ Unreal/UDeveloperSettings/UDeveloperSettings.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Engine/DeveloperSettings.h>
+
+#include "$safeitemname$.generated.h"
+
+UCLASS(config = !!!!)
+class U$safeitemname$ : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+
+};
